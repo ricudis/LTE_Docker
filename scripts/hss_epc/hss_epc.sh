e007a21945da43e1cf0f7aee4a314cedc118b422
Ubuntu <ubuntu@ubuntu.lan>
2020-06-22 01:30:20 +0000

    path fixes

--- scripts/hss_epc/hss_epc.sh
+++ scripts/hss_epc/hss_epc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-OPEN5GS_HSS_CONFIG=${LTE_BASE_DIR}/config/hss_epc/hss.yaml
-${LTE_BASE_DIR}/scripts/common/subst.sh ${OPEN5GS_HSS_CONFIG}.in ${OPEN5GS_HSS_CONFIG}
+OPEN5GS_HSS_EPC_CONFIG=${LTE_BASE_DIR}/open5gs/etc/open5gs/hss_epc.yaml
+${LTE_BASE_DIR}/scripts/common/subst.sh ${LTE_BASE_DIR}/config/hss_epc/hss_epc.yaml.in ${OPEN5GS_HSS_EPC_CONFIG}
 echo "nameserver $LTE_DNS_IP" > /etc/resolv.conf
-sh /opt/LTE/scripts/common/waitfor_mongo.sh
-/opt/LTE/open5gs/bin/open5gs-hssd -c ${OPEN5GS_HSS_CONFIG} 
+/opt/LTE/open5gs/bin/open5gs-hssd -c ${OPEN5GS_HSS_EPC_CONFIG}
--- scripts/sgw/sgw.sh
+++ scripts/sgw/sgw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OPEN5GS_SGW_CONFIG=${LTE_BASE_DIR}/config/sgw/sgw.yaml
-${LTE_BASE_DIR}/scripts/common/subst.sh ${OPEN5GS_SGW_CONFIG}.in ${OPEN5GS_SGW_CONFIG}
+OPEN5GS_SGW_CONFIG=${LTE_BASE_DIR}/open5gs/etc/open5gs/sgw.yaml
+${LTE_BASE_DIR}/scripts/common/subst.sh ${LTE_BASE_DIR}/config/sgw/sgw.yaml.in ${OPEN5GS_SGW_CONFIG}
 echo "nameserver $LTE_DNS_IP" > /etc/resolv.conf
-/opt/LTE/open5gs/bin/open5gs-sgwd -c ${OPEN5GS_SGW_CONFIG} 
+/opt/LTE/open5gs/bin/open5gs-sgwd -c ${OPEN5GS_SGW_CONFIG}
